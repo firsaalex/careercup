d5bc06fca2a88c4fcb7b99f12e896fb6c92af5ae
Alexey <firsaalex@gmail.com>
2015-07-08 19:48:00 +0300

    Iterator instead traversal

--- IteratorBST/main.cpp
+++ IteratorBST/main.cpp
@@ -2,6 +2,8 @@
 // Given a binary tree, implement an iterator that will iterate through its elements.
 
 #include <iostream>
+#include <queue>
+
 using namespace std;
 
 struct Node
@@ -11,59 +13,76 @@ struct Node
     Node *right = 0;
 };
 
-
-void binaryTreeInorderTraversal(struct Node *n)
+class Iterator
 {
-    if(n)
+public:
+    explicit Iterator(Node *ptr): ptr_(ptr)
     {
-        binaryTreeInorderTraversal(n->left);
-        cout << n->value << " ";
-        binaryTreeInorderTraversal(n->right);
+        if(ptr_)
+        {
+            if(ptr_->left) q.push(ptr_->left);
+            if(ptr_->right) q.push(ptr_->right);
+        }
     }
-}
 
-void binaryTreePreOrderTraversal(struct Node *n)
-{
-    if(n)
-    {
-        cout << n->value << " ";
-        binaryTreePreOrderTraversal(n->left);
-        binaryTreePreOrderTraversal(n->right);
-    }
-}
+    bool operator != (const Iterator &i) {return i.ptr_ != ptr_;}
+    bool operator == (const Iterator &i) {return i.ptr_ == ptr_;}
 
-void binaryTreePostorderTraversal(struct Node *n)
-{
-    if(n)
+    Node& operator *() {return *ptr_;}
+    Node* operator ->() {return ptr_;}
+
+    Iterator operator ++ ()
     {
-        binaryTreePostorderTraversal(n->left);
-        binaryTreePostorderTraversal(n->right);
-        cout << n->value << " ";
+        if(ptr_ && !q.empty())
+        {
+            ptr_ = q.front();
+            q.pop();
+
+            if(ptr_->left) q.push(ptr_->left);
+            if(ptr_->right) q.push(ptr_->right);
+        }
+        else ptr_ = nullptr;
+
+        return *this;
     }
-}
+private:
+    queue<Node*> q;
+    Node* ptr_;
+};
+
+class Tree
+{
+public:
+    explicit Tree (Node *r):root(r), iter(Iterator(root)){}
+    Iterator begin() {return Iterator(root);}
+    Iterator end() {return Iterator(nullptr);}
+private:
+    Node *root;
+    Iterator iter;
+};
 
 int main()
 {
- Node a,b,c,d,e,f;
+    Node a,b,c,d,e,f;
 
- a.value = 'a';
- a.left = &b;
- a.right = &c;
+    a.value = 'a';
+    a.left = &b;
+    a.right = &c;
+    b.value = 'b';
+    b.left = &d;
+    b.right = &e;
 
- b.value = 'b';
- b.left = &d;
- b.right = &e;
+    c.value = 'c';
+    c.right = &f;
 
- c.value = 'c';
- c.right = &f;
+    d.value = 'd';
+    e.value = 'e';
+    f.value = 'f';
 
- d.value = 'd';
- e.value = 'e';
- f.value = 'f';
+    Tree t(&a);
 
- binaryTreeInorderTraversal(&a);
- cout << endl;
- binaryTreePreOrderTraversal(&a);
- cout << endl;
- binaryTreePostorderTraversal(&a);
+    for (Iterator i = t.begin(); i != t.end(); ++i)
+        cout << (*i).value << " ";
+
+    return 0;
 }
